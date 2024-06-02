9ae090accdd57f25f62c416a353d81faf6e88e16
BManduca <brunnomanducarfe@gmail.com>
2024-06-02 13:00:44 -0300

    feat: implementing interact mode to add new coffees or more quantity coffees to order and remove | update value order in finish order

--- ProjectChallengeCoffeeDelivery/src/pages/CompleteOrder/components/CoffeeCartCard/index.tsx
+++ ProjectChallengeCoffeeDelivery/src/pages/CompleteOrder/components/CoffeeCartCard/index.tsx
@@ -8,12 +8,27 @@ import { QuantityInput } from '../../../../components/QuantityInput'
 import { Trash } from 'phosphor-react'
 import { CartItem } from '../../../../contexts/CartContext'
 import { formatMoney } from '../../../../utils/formatMoney'
+import { useCart } from '../../../../hooks/useCart'
 
 interface CoffeeCartCardProps {
   coffee: CartItem
 }
 
 export function CoffeeCartCard({ coffee }: CoffeeCartCardProps) {
+  const { changeCartItemQuantity, removeCartItem } = useCart()
+
+  function handleIncrease() {
+    changeCartItemQuantity(coffee.id, 'increase')
+  }
+
+  function handleDecrease() {
+    changeCartItemQuantity(coffee.id, 'decrease')
+  }
+
+  function handleRemove() {
+    removeCartItem(coffee.id)
+  }
+
   const coffeeTotal = coffee.price * coffee.quantity
   const formattedPrice = formatMoney(coffeeTotal)
   return (
@@ -23,8 +38,13 @@ export function CoffeeCartCard({ coffee }: CoffeeCartCardProps) {
         <div>
           <RegularText color="subtitle">{coffee.name}</RegularText>
           <ActionsContainer>
-            <QuantityInput size="small" quantity={coffee.quantity} />
-            <ButtonRemove>
+            <QuantityInput
+              size="small"
+              quantity={coffee.quantity}
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+            />
+            <ButtonRemove onClick={handleRemove}>
               <Trash size={16} />
               Remover
             </ButtonRemove>
